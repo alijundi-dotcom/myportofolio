4038bd035f2cd90e00acb3a78f618b7109fa5808
weladalah <ali.jundi@ui.ac.id>
2026-09-09 16:46:35 +0700

    Tutoral 2 Done + modif dikit

---
+++ main/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+# Create your views here.
+
+def show_main(request):
+    context = {
+        "name": "Ali Jundi Qowi",
+        "npm": "2506611585",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "Mahasiswa Ilmu Komputer Universitas Indonesia yang tertarik "
+            "pada pengembangan perangkat lunak dan pendidikan."
+            "Mahasiswa yang juga tertarik gaya gravitasi bumi"
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Ali",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
--- portofolio/urls.py
+++ portofolio/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from portofolio.views import landing_page
 from portofolio.views import burhan_page
@@ -24,6 +24,7 @@ from portofolio.views import exp_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("main.urls")),
     path('', landing_page, name ='index'),
     path('burhan/',burhan_page, name="burhan"),
     path('skills/', skills_page, name= 'skills' ),
